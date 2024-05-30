aca394aa415bb26081564ff317294e11741da249
Shilpa Sahu <shlpasahu335@gmail.com>
2024-05-30 23:36:53 +0530

    game

--- TicTacToeGame-3/TicTacToe.java
+++ TicTacToeGame-3/TicTacToe.java
@@ -1,1 +1,99 @@
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TicTacToe extends JFrame implements ActionListener {
+
+    private JButton[][] buttons = new JButton[3][3];
+    private boolean playerXTurn = true;
+
+    public TicTacToe() {
+        setTitle("Tic-Tac-Toe");
+        setSize(400, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridLayout(3, 3));
+
+        initializeButtons();
+    }
+
+    private void initializeButtons() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j] = new JButton("");
+                buttons[i][j].setFont(new Font("Arial", Font.PLAIN, 60));
+                buttons[i][j].setFocusPainted(false);
+                buttons[i][j].addActionListener(this);
+                add(buttons[i][j]);
+            }
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton clickedButton = (JButton) e.getSource();
+        if (!clickedButton.getText().equals("")) {
+            return;
+        }
+
+        if (playerXTurn) {
+            clickedButton.setText("X");
+        } else {
+            clickedButton.setText("O");
+        }
+
+        playerXTurn = !playerXTurn;
+        checkForWinner();
+    }
+
+    private void checkForWinner() {
+        String[][] board = new String[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = buttons[i][j].getText();
+            }
+        }
+
+        // Check rows, columns, and diagonals
+        for (int i = 0; i < 3; i++) {
+            if (checkLine(board[i][0], board[i][1], board[i][2]) ||
+                    checkLine(board[0][i], board[1][i], board[2][i])) {
+                announceWinner(board[i][i]);
+                return;
+            }
+        }
+        if (checkLine(board[0][0], board[1][1], board[2][2]) ||
+                checkLine(board[0][2], board[1][1], board[2][0])) {
+            announceWinner(board[1][1]);
+        }
+    }
+
+    private boolean checkLine(String s1, String s2, String s3) {
+        return s1.equals(s2) && s2.equals(s3) && !s1.equals("");
+    }
+
+    private void announceWinner(String winner) {
+        JOptionPane.showMessageDialog(this, "Player " + winner + " wins!");
+        resetBoard();
+    }
+
+    private void resetBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setText("");
+            }
+        }
+        playerXTurn = true;
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            TicTacToe game = new TicTacToe();
+            game.setVisible(true);
+        });
+    }
+}
+
+
+
